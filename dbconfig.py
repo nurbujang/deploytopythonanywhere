dfa26a4e0511c710583f44657baf43a78d70f828
nurbujang <123871987+nurbujang@users.noreply.github.com>
2024-04-14 21:14:20 +0100

    requirements, dbconfig.py

--- dbconfig.py
+++ dbconfig.py
@@ -1,6 +1,6 @@
 mysql = {
-    'host':"localhost",
-    'user':"root",
-    'password':"",
-    'database':"datarepresentation"
+    'host':"nurbujang.mysql.pythonanywhere-services.com",
+    'user':"nurbujang",
+    'password':"wsaapassword",
+    'database':"nurbujang$wsaa"
 }
--- dbconfigpa.py
+++
@@ -1,6 +0,0 @@
-mysql = {
-    'host':"nurbujang.mysql.pythonanywhere-services.com",
-    'user':"nurbujang",
-    'password':"wsaapassword",
-    'database':"nurbujang$wsaa"
-}
